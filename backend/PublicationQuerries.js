fb2b2431a648eb623c54eeb2179b27ca4e258637
stbn1699 <stbnbasson@gmail.com>
2024-03-23 11:52:12 +0100

    added some more

--- backend/PublicationQuerries.js
+++ backend/PublicationQuerries.js
@@ -8,6 +8,9 @@ const pool = new Pool({
 })
 
 const getPublications = (request, response) => {
+
+    console.log(`getting all publications...`)
+
     const query = `
         SELECT publications.*, 
                (SELECT COUNT(*) 
@@ -17,15 +20,20 @@ const getPublications = (request, response) => {
 
     pool.query(query, (error, results) => {
         if (error) {
-            console.log(error);
+            console.log("error while getting publications")
+            console.log(error + "\n\n");
             response.status(500).json({error: 'An error occurred while getting the publications'});
         } else {
+            console.log("publications retrieved\n\n")
             response.status(200).json(results.rows);
         }
     });
 }
 
 const getPublicationById = (request, response) => {
+
+    console.log(`getting publication ${request.params.id}...`)
+
     const id = request.params.id;
     const query = `
         SELECT publications.*, 
@@ -37,10 +45,11 @@ const getPublicationById = (request, response) => {
 
     pool.query(query, [id], (error, results) => {
         if (error) {
-            console.log(error);
+            console.log("error while getting publication")
+            console.log(error + "\n\n");
             response.status(500).json({error: 'An error occurred while getting the publication'});
         } else {
-            console.log(`getting publication ${results.rows[0].title}\n\n`)
+            console.log(`publication ${results.rows[0].title} retrieved\n\n`)
             response.status(200).json(results.rows[0]);
         }
     });
@@ -49,11 +58,15 @@ const getPublicationById = (request, response) => {
 const addRecipe = (request, response) => {
     const { title, uuid, info_1, info_2, content } = request.body;
 
+    console.log(`adding recipe ${title}...`)
+
     const getUsernameQuery = 'SELECT username FROM accounts WHERE uuid = $1';
 
     pool.query(getUsernameQuery, [uuid], (error, results) => {
         if (results.rows.length > 0) {
             const author = results.rows[0].username;
+
+            console.log(`user found : ${author}\n`)
 
             const addRecipeQuery = `
                 INSERT INTO publications (title, author, info_1, info_2, content)
@@ -63,7 +76,8 @@ const addRecipe = (request, response) => {
 
             pool.query(addRecipeQuery, [title, author, info_1, info_2, content], (error, results) => {
                 if (error) {
-                    console.log(error);
+                    console.log("error while adding recipe")
+                    console.log(error + "\n\n");
                     response.status(500).json({error: 'An error occurred while adding the recipe'});
                 } else {
                     const publicationUuid = results.rows[0].uuid;
@@ -72,6 +86,7 @@ const addRecipe = (request, response) => {
                 }
             });
         } else {
+            console.log("user not found\n\n")
             response.status(404).json({error: 'User not found'});
         }
     });
